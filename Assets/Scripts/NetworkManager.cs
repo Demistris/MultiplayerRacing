e7b681b6741c525dff297f7bec83b6e9af9958e3
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-14 14:39:37 +0200

    Imported photon and assets

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private InputField _playerNameInput;
+
+    #region UI Callback Methods
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = _playerNameInput.text;
+
+        if(string.IsNullOrEmpty(playerName))
+        {
+            Debug.Log("Player name is invalid.");
+            return;
+        }
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to internet.");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon.");
+    }
+
+    #endregion
+}
