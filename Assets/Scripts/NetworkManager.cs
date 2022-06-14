d177321233032ad9936c577af0d9ab742778d85e
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-14 16:05:46 +0200

    Added UI navigation

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -5,6 +5,28 @@ using Photon.Pun;
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _playerNameInput;
+    [SerializeField] private GameObject _loginUIPanel;
+    [SerializeField] private GameObject _connectingInfoUIPanel;
+    [SerializeField] private GameObject _createRoomUIPanel;
+    [SerializeField] private GameObject _creatingRoomInfoUIPanel;
+    [SerializeField] private GameObject _gameOptionsUIPanel;
+    [SerializeField] private GameObject _insideRoomUIPanel;
+    [SerializeField] private GameObject _joinRandomRoomUIPanel;
+
+    private void Start()
+    {
+        ActivatePanel(_loginUIPanel.name);
+    }
+
+    public void ActivatePanel(string panelNameToBeActivated)
+    {
+        _loginUIPanel.SetActive(_loginUIPanel.name.Equals(panelNameToBeActivated));
+        _connectingInfoUIPanel.SetActive(_connectingInfoUIPanel.name.Equals(panelNameToBeActivated));
+        _createRoomUIPanel.SetActive(_createRoomUIPanel.name.Equals(panelNameToBeActivated));
+        _creatingRoomInfoUIPanel.SetActive(_creatingRoomInfoUIPanel.name.Equals(panelNameToBeActivated));
+        _gameOptionsUIPanel.SetActive(_gameOptionsUIPanel.name.Equals(panelNameToBeActivated));
+        _joinRandomRoomUIPanel.SetActive(_joinRandomRoomUIPanel.name.Equals(panelNameToBeActivated));
+    }
 
     #region UI Callback Methods
 
@@ -18,11 +40,18 @@ public class NetworkManager : MonoBehaviourPunCallbacks
             return;
         }
 
+        ActivatePanel(_connectingInfoUIPanel.name);
+
         if (!PhotonNetwork.IsConnected)
         {
             PhotonNetwork.LocalPlayer.NickName = playerName;
             PhotonNetwork.ConnectUsingSettings();
         }
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        ActivatePanel(_gameOptionsUIPanel.name);
     }
 
     #endregion
@@ -37,6 +66,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon.");
+        ActivatePanel(_gameOptionsUIPanel.name);
     }
 
     #endregion
