82c856b57abaf8aba24806621ebbea714c99085a
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-15 12:31:34 +0200

    Creating rooms based on game mode type - custom room properties

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _playerNameInput;
+    [SerializeField] private InputField _roomNameInput;
+
     [SerializeField] private GameObject _loginUIPanel;
     [SerializeField] private GameObject _connectingInfoUIPanel;
     [SerializeField] private GameObject _createRoomUIPanel;
@@ -12,6 +15,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject _gameOptionsUIPanel;
     [SerializeField] private GameObject _insideRoomUIPanel;
     [SerializeField] private GameObject _joinRandomRoomUIPanel;
+
+    private string _gameMode;
 
     private void Start()
     {
@@ -26,6 +31,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         _creatingRoomInfoUIPanel.SetActive(_creatingRoomInfoUIPanel.name.Equals(panelNameToBeActivated));
         _gameOptionsUIPanel.SetActive(_gameOptionsUIPanel.name.Equals(panelNameToBeActivated));
         _joinRandomRoomUIPanel.SetActive(_joinRandomRoomUIPanel.name.Equals(panelNameToBeActivated));
+    }
+
+    public void SetGameMode(string gameMode)
+    {
+        _gameMode = gameMode;
     }
 
     #region UI Callback Methods
@@ -54,6 +64,39 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         ActivatePanel(_gameOptionsUIPanel.name);
     }
 
+    public void OnCreateRoomButtonClicked()
+    {
+        if(_gameMode == null)
+        {
+            return;
+        }
+
+        ActivatePanel(_creatingRoomInfoUIPanel.name);
+
+        string roomName = _roomNameInput.text;
+
+        if(string.IsNullOrEmpty(roomName))
+        {
+            roomName = "Room " + Random.Range(1000, 10000);
+        }
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 3;
+
+        string[] roomPropertiesInLobby = { "gm" }; //gm = game mode
+
+        //Two game modes
+        //1. Racing = rc
+        //2. Death race = dr
+
+        ExitGames.Client.Photon.Hashtable customRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "gm", _gameMode } };
+
+        roomOptions.CustomRoomPropertiesForLobby = roomPropertiesInLobby;
+        roomOptions.CustomRoomProperties = customRoomProperties;
+
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+    }
+
     #endregion
 
     #region Photon Callbacks
@@ -67,6 +110,26 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon.");
         ActivatePanel(_gameOptionsUIPanel.name);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + " is created.");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
+
+        if(PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("gm"))
+        {
+            object gameModeName;
+
+            if(PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("gm", out gameModeName))
+            {
+                Debug.Log(gameModeName.ToString());
+            }
+        }
     }
 
     #endregion
