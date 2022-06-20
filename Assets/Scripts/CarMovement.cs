351d519e3ad86096f900ae3b7c08dba441de06e8
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-20 12:31:13 +0200

    Added car movement

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private Vector3 _thrustForce = new Vector3(0f, 0f, 45f);
+    private Vector3 _rotationTorque = new Vector3(0f, 8f, 0f);
+
+    private void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            _rigidbody.AddRelativeForce(_thrustForce);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            _rigidbody.AddRelativeForce(-_thrustForce);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            _rigidbody.AddRelativeTorque(-_rotationTorque);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            _rigidbody.AddRelativeTorque(_rotationTorque);
+        }
+    }
+}
