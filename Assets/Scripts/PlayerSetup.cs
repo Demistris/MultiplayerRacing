3974314a6b6d79e88bd8f6d50fad7203ab984f4c
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-20 13:27:06 +0200

    Added player setup and synchronizing movement across the network

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private CarMovement _carMovement;
+    [SerializeField] private Camera _camera;
+
+    private void Start()
+    {
+        if(photonView.IsMine)
+        {
+            _carMovement.enabled = true;
+            _camera.enabled = true;
+            return;
+        }
+
+        _carMovement.enabled = false;
+        _camera.enabled = false;
+    }
+}
