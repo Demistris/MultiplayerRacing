3f93958fdde4b2e9fc846350e44ea93421bb7aed
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-20 11:42:59 +0200

    Player selection UI

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _selectablePlayers;
+    [SerializeField] private int _playerSelectionNumber = 0;
+
+    private void Start()
+    {
+        ActivatePlayer(_playerSelectionNumber);
+    }
+
+    public void NextPlayer()
+    {
+        _playerSelectionNumber += 1;
+
+        if(_playerSelectionNumber >= _selectablePlayers.Length)
+        {
+            _playerSelectionNumber = 0;
+        }
+
+        ActivatePlayer(_playerSelectionNumber);
+    }
+
+    public void PreviousPlayer()
+    {
+        _playerSelectionNumber -= 1;
+
+        if (_playerSelectionNumber < 0)
+        {
+            _playerSelectionNumber = _selectablePlayers.Length - 1;
+        }
+
+        ActivatePlayer(_playerSelectionNumber);
+    }
+
+    private void ActivatePlayer(int x)
+    {
+        foreach(GameObject selectablePlayer in _selectablePlayers)
+        {
+            selectablePlayer.SetActive(false);
+        }
+
+        _selectablePlayers[x].SetActive(true);
+    }
+}
