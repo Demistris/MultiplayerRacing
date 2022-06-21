6368ac39aa8b936c7a42be889fe9a5aab1e331d4
Kaja Więckowska <k.wieckowska66@gmail.com>
2022-06-21 13:20:03 +0200

    Added shooting

---
+++ Assets/Scripts/MachineGunBullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MachineGunBullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+
+    public void Initialize(Vector3 direction, float speed, float damage)
+    {
+        transform.forward = direction;
+        _rigidbody.velocity = direction * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private MachineGunBullet _bulletPrefab;
+    [SerializeField] private Transform _firePosition;
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private DeathRacePlayer _deathRacePlayerProperties;
+
+    private void Update()
+    {
+        if(Input.GetKey("space"))
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        Ray ray = _playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+
+        MachineGunBullet bullet = Instantiate(_bulletPrefab, _firePosition.position, Quaternion.identity);
+        bullet.Initialize(ray.direction, _deathRacePlayerProperties.BulletSpeed, _deathRacePlayerProperties.Damage);
+    }
+}
